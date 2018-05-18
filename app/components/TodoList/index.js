f55ee94fec3dccab59add40b582d3e37ff6dff8f
Elliott Thompson <elliott@moo.com>
2018-05-18 11:21:28 +0100

    ToDo List test

---
+++ app/components/TodoList/index.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * TodoList
+ *
+ */
+
+import { Box } from 'bloomer';
+
+import React from 'react';
+// import styled from 'styled-components';
+
+function TodoList() {
+  return <Box>TEST</Box>;
+}
+
+TodoList.propTypes = {};
+
+export default TodoList;
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,15 +10,16 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
 
-export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+import TodoList from 'components/TodoList/Loadable';
+
+export default class HomePage extends React.PureComponent {
+  // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div>
+        <TodoList />
+      </div>
     );
   }
 }
